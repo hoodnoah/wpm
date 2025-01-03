7e193885caefaecce62916cf6f3124bed4563c9c
Noah Hood <hood.noah@icloud.com>
2025-01-03 15:36:30 -0500

    add lipgloss styling

--- constants/constants.go
+++ constants/constants.go
@@ -1,5 +1,7 @@
 package constants
 
+import "github.com/charmbracelet/lipgloss"
+
 const (
 	Header = `
  _    __________  ___
@@ -9,4 +11,10 @@ const (
 \  /\  / |   | |  | |
  \/  \/\_|   \_|  |_/
                     `
+)
+
+var (
+	TimeStyle      = lipgloss.NewStyle().Foreground(lipgloss.Color("#33FF57"))
+	WordCountStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#FFD700"))
+	WPMStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color("#ef7c8e"))
 )
